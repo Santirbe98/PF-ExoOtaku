2dfd417375c7212b56ff603026e9693232af8a73
Santiago Bonetto <105248137+Santirbe98@users.noreply.github.com>
2022-12-02 19:38:07 -0300

    Update Product.js

--- api/src/models/Product.js
+++ api/src/models/Product.js
@@ -4,11 +4,6 @@ module.exports = (sequelize) => {
 
   sequelize.define("product", 
   {
-    product_id:{
-      type: DataTypes.UUID,
-      primaryKey: true,
-      defaultValue: DataTypes.UUIDV4,
-    },
 
     name: {
       type: DataTypes.STRING,
