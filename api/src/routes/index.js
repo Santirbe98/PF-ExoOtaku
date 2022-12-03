d8537263621dabfcc8dffe40980b6416339aef1d
AcevedoLeandro <leo9.neo@gmail.com>
2022-12-03 01:55:19 -0300

    feat(api/routes/types): GET Y POST de types

--- api/src/routes/index.js
+++ api/src/routes/index.js
@@ -2,6 +2,7 @@ const { Router } = require("express");
 // Importar todos los routers;
 const productsRouter = require("./products.js");
 const colorRouter = require("./color.js");
+const typesRouter = require("./types.js");
 // Ejemplo: const authRouter = require('./auth.js');
 
 const router = Router();
@@ -9,5 +10,5 @@ const router = Router();
 // Configurar los routers
 router.use("/products", productsRouter);
 router.use("/color", colorRouter);
-
+router.use("/types", typesRouter);
 module.exports = router;
