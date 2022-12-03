640e5df632937a17582fbaeca05f66da2ed23161
Gabriel Iciarte <gabriel_viki@hotmail.com>
2022-12-03 02:51:33 -0300

    feat(api/controller/):refactor controller route

--- api/src/routes/products.js
+++ api/src/routes/products.js
@@ -1,44 +1,66 @@
+
 const { Router } = require("express");
 const router = Router();
 
 // const data = require('../data')
-const { getAllProducts, createNewProduct, getProductDetail, modifyProd, deleteProd } = require('./Controllers/productsController')
-
+const {
+  getAllProducts,
+  createNewProduct,
+  getProductDetail,
+  modifyProd,
+  deleteProd,
+} = require("./Controllers/productsController");
 
 router.get("/", async (req, res) => {
-  const { id } = req.query
+  const name  = req.query.name;
   try {
-    let listProducts
-    let productListres = await getAllProducts()
-    if (id) {
-      listProducts = productListres.filter(el => el.product_id.includes(id))
-      res.status(200).send(listProducts)
-    }
-
-    else {
-      res.status(200).send(productListres)
+    let listProducts;
+    let productListres = await getAllProducts();
+    console.log(listProducts);
+    if (name) {
+      listProducts = productListres.filter((el) =>
+        el.name.toLowerCase().includes(name.toLowerCase())
+      );
+      name.length
+        ? res.status(200).send(listProducts)
+        : res.status(404).send("This product doesn't exist");
+    } else {
+      res.status(200).send(productListres);
     }
   } catch (error) {
-    console.error({ error: error.message })
-    res.status(500).send('Products not found')
+    console.error({ error: error.message });
+    res.status(500).send("Products not found");
   }
-})
+});
 
-router.post('/', async (req, res) => {
-  const { name, price, descriptions, images, stock, color } = req.body
-  try {
-    let newProduct = await createNewProduct({ name, price, descriptions, images, stock, color })
-    res.status(201).send(newProduct)
-  } catch (error) {
-    console.error({ error: error.message })
+router.post("/", async (req, res) => {
+  const { name, price, descriptions, images, stock, color, size, type } =
+    req.body;
+  if (
+    !name ||
+    !price ||
+    !descriptions ||
+    !images ||
+    !stock ||
+    !color ||
+    !size ||
+    !type
+  ) {
+    res.status(400).send("Complete all required fields");
   }
-})
+  try {
+    const newProduct = await createNewProduct(req.body);
+    res.status(201).send(newProduct);
+  } catch (error) {
+    res.status(400).json({ error: error.message });
+  }
+});
 
 router.get("/:id", async (req, res) => {
   const { id } = req.params;
   try {
-    let productDetail = await getProductDetail(id)
-    res.status(200).send(productDetail)
+    let productDetail = await getProductDetail(id);
+    res.status(200).send(productDetail);
   } catch (error) {
     console.error({ error: error });
     return res.status(404).send("Product not found");
@@ -47,26 +69,27 @@ router.get("/:id", async (req, res) => {
 
 router.put("/:id", async (req, res) => {
   const { id } = req.params;
-  const {name, price, descriptions, images, stock} = req.body
+  const { name, price, descriptions, images, stock } = req.body;
   try {
-    await modifyProd({id, name, price, descriptions, images, stock})
-    res.status(200).send('Product modified successfully')
+    await modifyProd({ id, name, price, descriptions, images, stock });
+    res.status(200).send("Product modified successfully");
   } catch (error) {
     console.error({ error: error });
     return res.status(404).send("Product not found");
   }
 });
 
-router.delete('/:id', async(req, res) =>  {
-  const {id} = req.params
+router.delete("/:id", async (req, res) => {
+  const { id } = req.params;
   try {
-    let response = await deleteProd(id)
-    res.status(200).send(response)
+    let response = await deleteProd(id);
+    res.status(200).send(response);
   } catch (error) {
     console.error({ error: error });
     return res.status(404).send(error.message);
   }
-})
-
+});
 
 module.exports = router;
+
+
