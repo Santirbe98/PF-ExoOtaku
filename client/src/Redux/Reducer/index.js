c6db24042a717c1467625b92d8bf4e2da6e621a3
Santirbe98 <santirbe@gmail.com>
2022-12-04 19:42:11 -0300

    fix(client/redux/reducer): filtros arreglados

--- client/src/Redux/Reducer/index.js
+++ client/src/Redux/Reducer/index.js
@@ -13,7 +13,6 @@ const initialState = {
   // loading: true,
 };
 
-//agregar cada CASE de cada Actions
 function rootReducer(state = initialState, action) {
   switch (action.type) {
     case GET_PRODUCTS:
@@ -21,51 +20,26 @@ function rootReducer(state = initialState, action) {
         ...state,
         products: action.payload,
         filterProducts: action.payload,
-        // loading: false,
       };
     case FILTER_ALL:
       const allProducts = state.products;
-      const {color, type, category} = action.payload;
-       // let color = "All";
-      // let type = "All";
-      // let category = "All";
-      // if (
-      //   action.payload === "Black" ||
-      //   action.payload === "White" ||
-      //   action.payload === "Blue" ||
-      //   action.payload === "Pink"
-      // ) {
-      //   color = action.payload;
-      // }
-      // if (action.payload === "Tshirt" || action.payload === "Sweter") {
-      //   type = action.payload;
-      // }
+      const { color, type, category } = action.payload;
 
-      // if (action.payload !== "All") {
-      //   category = action.payload;
-      // }
-      // category = action.payload;
       const filterProducts =
-        // color === "All"
-        //   ? allProducts
-        //   : allProducts.filter((p) => p.color === color);
-          category === "All"
+        category === "All"
           ? allProducts
           : allProducts.filter((p) => p.category === category);
-          console.log(filterProducts, 'primer filtro category')
+      console.log(filterProducts, "primer filtro category");
       const filterProducts2 =
         type === "All"
           ? filterProducts
-          : filterProducts.filter((p) => p.type[0] === type);
-          console.log(filterProducts2, 'segundo filtro type')
+          : filterProducts.filter((p) => p.type.find((t) => t === type));
+      console.log(filterProducts2, "segundo filtro type");
       const filterProducts3 =
-        // category === "All"
-        //   ? filterProducts2
-        //   : filterProducts2.filter((p) => p.category === category);
-          color === "All"
+        color === "All"
           ? filterProducts2
-          : filterProducts2.filter((p) => p.color[0] === color);
-          console.log(filterProducts3, 'primer filtro color')
+          : filterProducts2.filter((p) => p.color.find((c) => c === color));
+      console.log(filterProducts3, "primer filtro color");
       return {
         ...state,
         filterProducts: filterProducts3,
