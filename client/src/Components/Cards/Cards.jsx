0eb2db0de388b4efa3486659c7a961d136aaf0be
AcevedoLeandro <leo9.neo@gmail.com>
2022-12-04 01:23:18 -0300

    feat(components/Cards)CrearComponenteCards

---
+++ client/src/Components/Cards/Cards.jsx
@@ -0,0 +1,25 @@
+import { React, useEffect } from "react";
+import { getProducts } from "../../Redux/Actions";
+import { Card } from './prueba.jsx'
+import { useDispatch, useSelector } from "react-redux";
+
+export function Cards() {
+    let dispatch = useDispatch();
+    let products = useSelector(state => state.products)
+
+    useEffect(() => {
+        if (!products.length) dispatch(getProducts())
+    }, [dispatch]);
+
+    return (
+        <div>
+            {products?.map(p => <Card
+                key={p.id}
+                image={p.images}
+                name={p.name}
+                category={p.categories[0].category}
+                price={p.price} />
+            )}
+        </div>
+    )
+}
