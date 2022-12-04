38433a1774dad2c0e459da35d537d3d4e6a7689b
Santirbe98 <santirbe@gmail.com>
2022-12-04 09:52:32 -0300

    feat(client/components): Archivos y carpetas base

--- client/src/App.js
+++ client/src/App.js
@@ -1,18 +1,16 @@
 import React from "react";
 import { Switch, Route } from "react-router-dom";
 import "./App.css";
+import { Form } from "./Components/Form/Form";
+import { Home } from "./Components/Home/Home";
 
 export default function App() {
   return (
-    <Switch>
-      {
-        <Route exact path="/home" component={Cards} />
-        /* <Route exact path=""component={LandingPage} />
-      <Route exact path="" component={} />
-      <Route exact path="" component={} />
-      <Route exact path="" component={} />
-      <Route  path="*" component={NotFound} /> */
-      }
-    </Switch>
+    <div className="App">
+      <Switch>
+        <Route exact path="/home" component={Home} />
+        <Route exact path="/form" component={Form} />
+      </Switch>
+    </div>
   );
 }
