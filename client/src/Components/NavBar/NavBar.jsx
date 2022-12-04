1e5d9933f5c5d9aabe3da8862eb8ce7837cfc449
juanetem3n <juanmanuelalcaide@gmail.com>
2022-12-04 16:21:59 -0300

    Feat(Client/src/NavBar):LogicaNavBar

--- client/src/Components/NavBar/NavBar.jsx
+++ client/src/Components/NavBar/NavBar.jsx
@@ -1,5 +1,41 @@
 import React from "react";
+import { SearchBar } from "../SearchBar/SearchBar.jsx";
+import { Link } from "react-router-dom"
+import "./NavBar.css"
+
 
 export const NavBar = () => {
-  return <div>NavBar</div>;
+  return (
+    <div className="divSection">
+      <section>
+        <img alt="no found" />
+      </section>
+      <section>
+        <SearchBar />
+      </section>
+      <section>
+        <Link to="/home">
+          <button> Home</button>
+        </Link>
+      </section>
+      <section>
+        <Link to="/about">
+          <button> About</button>
+        </Link>
+      </section>
+      <section>
+        <Link to="/shop">
+          <button> Shop</button>
+        </Link>
+      </section>
+      <section>
+        <Link to="/help">
+          <button> Help</button>
+        </Link>
+      </section>
+      <section>
+        <button>Your Cart</button>
+      </section>
+    </div>
+  );
 };
