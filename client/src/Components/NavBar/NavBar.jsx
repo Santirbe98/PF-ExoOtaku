d2b540c1a7377e030df29fef84d6799063b13947
Salvaarmanasco <salvador.armanasco@gmail.com>
2022-12-05 17:14:56 -0300

    css-colocando/navbar y footer - salvaarmanasco

--- client/src/Components/NavBar/NavBar.jsx
+++ client/src/Components/NavBar/NavBar.jsx
@@ -23,7 +23,7 @@ export const NavBar = () => {
                     d="M24 12l-5.657 5.657-1.414-1.414L21.172 12l-4.243-4.243 1.414-1.414L24 12zM2.828 12l4.243 4.243-1.414 1.414L0 12l5.657-5.657L7.07 7.757 2.828 12zm6.96 9H7.66l6.552-18h2.128L9.788 21z"
                     fill="currentColor"
                   ></path>
-                </svg>{" "}
+                </svg>
                 Home
               </span>
             </button>
@@ -46,20 +46,20 @@ export const NavBar = () => {
           </Link>
         </section>
         <section>
-          <Link to="/shop">
-            <button className={s.navButton}>
-              <span>
-                <svg viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
-                  <path d="M0 0h24v24H0z" fill="none"></path>
-                  <path
-                    d="M24 12l-5.657 5.657-1.414-1.414L21.172 12l-4.243-4.243 1.414-1.414L24 12zM2.828 12l4.243 4.243-1.414 1.414L0 12l5.657-5.657L7.07 7.757 2.828 12zm6.96 9H7.66l6.552-18h2.128L9.788 21z"
-                    fill="currentColor"
-                  ></path>
-                </svg>
-                Shop
-              </span>
-            </button>
-          </Link>
+          {/* <Link to="/shop"> */}
+          <button className={s.navButton}>
+            <span>
+              <svg viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
+                <path d="M0 0h24v24H0z" fill="none"></path>
+                <path
+                  d="M24 12l-5.657 5.657-1.414-1.414L21.172 12l-4.243-4.243 1.414-1.414L24 12zM2.828 12l4.243 4.243-1.414 1.414L0 12l5.657-5.657L7.07 7.757 2.828 12zm6.96 9H7.66l6.552-18h2.128L9.788 21z"
+                  fill="currentColor"
+                ></path>
+              </svg>
+              Shop
+            </span>
+          </button>
+          {/* </Link> */}
         </section>
         <section>
           <Link to="/help">
@@ -91,10 +91,6 @@ export const NavBar = () => {
             </span>
           </button>
         </section>
-      </div>
-
-      <div>
-        <Filter />
       </div>
     </div>
   );
