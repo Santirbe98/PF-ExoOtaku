eec81d5252d6a151b3fa81435a6ec4eec354f3a7
Salvaarmanasco <salvador.armanasco@gmail.com>
2022-12-06 16:49:43 -0300

    categoryEnCards-salvaarmanasco

--- client/src/Components/Card/Card.jsx
+++ client/src/Components/Card/Card.jsx
@@ -2,7 +2,7 @@ import React from "react";
 import { Link } from "react-router-dom";
 import s from "./Card.module.css";
 
-export function Card({ name, price, image, id }) {
+export function Card({ name, price, image, id, category }) {
   return (
     <div className={s.containerCard}>
       <Link
@@ -22,6 +22,7 @@ export function Card({ name, price, image, id }) {
         </div>
         <div className={s.infoContainer}>
           <h2>{name}</h2>
+          <h4>{category}</h4>
           <h2>$ {price}</h2>
         </div>
       </Link>
