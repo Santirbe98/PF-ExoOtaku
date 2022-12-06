874a6c0912fef851830946ef191a4921285b45c3
Salvaarmanasco <salvador.armanasco@gmail.com>
2022-12-06 16:52:42 -0300

    fix-cards&&form

--- client/src/Components/NavBar/NavBar.jsx
+++ client/src/Components/NavBar/NavBar.jsx
@@ -45,8 +45,8 @@ export const NavBar = () => {
             </button>
           </Link>
         </section>
-        <section>
-          {/* <Link to="/shop"> */}
+        {/* <section>
+          <Link to="/shop">
           <button className={s.navButton}>
             <span>
               <svg viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
@@ -59,7 +59,23 @@ export const NavBar = () => {
               Shop
             </span>
           </button>
-          {/* </Link> */}
+          </Link>
+        </section> */}
+        <section>
+          <Link to="/form">
+            <button className={s.navButton}>
+              <span>
+                <svg viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
+                  <path d="M0 0h24v24H0z" fill="none"></path>
+                  <path
+                    d="M24 12l-5.657 5.657-1.414-1.414L21.172 12l-4.243-4.243 1.414-1.414L24 12zM2.828 12l4.243 4.243-1.414 1.414L0 12l5.657-5.657L7.07 7.757 2.828 12zm6.96 9H7.66l6.552-18h2.128L9.788 21z"
+                    fill="currentColor"
+                  ></path>
+                </svg>
+                Form
+              </span>
+            </button>
+          </Link>
         </section>
         <section>
           <Link to="/help">
