e57bf0f62820fb12d4c029bc6bc1474bbbd4338d
Santirbe98 <santirbe@gmail.com>
2022-12-12 15:44:06 -0300

    DB_URL add

--- api/src/db.js
+++ api/src/db.js
@@ -2,15 +2,12 @@ require("dotenv").config();
 const { Sequelize } = require("sequelize");
 const fs = require("fs");
 const path = require("path");
-const { DB_USER, DB_PASSWORD, DB_HOST } = process.env;
+const { DB_USER, DB_PASSWORD, DB_HOST, DB_URL } = process.env;
 
-const sequelize = new Sequelize(
-  `postgres://${DB_USER}:${DB_PASSWORD}@${DB_HOST}/exOtaku`,
-  {
-    logging: false,
-    native: false,
-  }
-);
+const sequelize = new Sequelize(DB_URL, {
+  logging: false,
+  native: false,
+});
 const basename = path.basename(__filename);
 
 const modelDefiners = [];
