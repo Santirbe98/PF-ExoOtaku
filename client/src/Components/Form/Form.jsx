c9fc60f48347a4c6ef0b2883ac94b8f6dd51d8dc
AcevedoLeandro <leo9.neo@gmail.com>
2022-12-12 00:14:49 -0300

    Refactor(client-Form)Implementacion de Cloudinary

--- client/src/Components/Form/Form.jsx
+++ client/src/Components/Form/Form.jsx
@@ -1,40 +1,51 @@
-import React from "react";
-import { Link } from "react-router-dom";
-import { validate } from "./Validate";
-import { useState } from "react";
+import React, { useState } from "react";
 import { useDispatch } from "react-redux";
 import { postProduct } from "../../Redux/Actions";
-import { NavBar } from "../NavBar/NavBar.jsx";
 
+import axios from "axios";
+import { NavBar } from "../NavBar/NavBar.jsx";
+import { makeStyles } from "@material-ui/core/styles";
+import Button from "@material-ui/core/Button";
+import { validate } from "./Validate";
 import style from "./Form.module.css";
-import * as T from "./Errors.module.css";
+// import * as T from "./Errors.module.css";
+
+import Radio from "@mui/material/Radio";
+import RadioGroup from "@mui/material/RadioGroup";
+import FormControlLabel from "@mui/material/FormControlLabel";
+import FormControl from "@mui/material/FormControl";
+import FormLabel from "@mui/material/FormLabel";
+import InputLabel from "@material-ui/core/InputLabel";
+import Input from "@material-ui/core/Input";
+import { FormHelperText, MenuItem, Select } from "@mui/material";
+
+const useStyles = makeStyles((theme) => ({
+  root: {
+    "& > *": {
+      margin: theme.spacing(1),
+    },
+  },
+  input: {
+    display: "none",
+  },
+}));
 
 export const Form = () => {
-  const dispatch = useDispatch();
-  const [errors, setErrors] = useState({});
-  const [input, setInput] = useState({
-    name: "",
-    price: 0,
-    descriptions: "",
-    images: [],
-    stock: 0,
-    type: "",
-    size: ["XSmall", "Small", "Medium", "Large", "XLarge", "XXLarge"],
-    color: ["Black", "White"],
-    category: "",
-  });
-
   const initialState = {
     name: "",
     price: 0,
     descriptions: "",
     images: [],
     stock: 0,
-    color: ["Black", "White"],
+    color: [],
     type: "",
     size: ["XSmall", "Small", "Medium", "Large", "XLarge", "XXLarge"],
     category: "",
   };
+
+  const dispatch = useDispatch();
+  const [errors, setErrors] = useState({});
+  const [input, setInput] = useState(initialState);
 
   function handleSubmit(e) {
     e.preventDefault();
@@ -56,15 +67,12 @@ export const Form = () => {
     );
   }
 
-  function handleImages(e) {
-    setInput({
-      ...input,
-      images: [...input.images, String(e.target.value)],
-    });
+  function addcolor(e) {
+    setInput({ ...input, color: [e.target.value] });
     setErrors(
       validate({
         ...input,
-        images: e.target.value,
+        [e.target.name]: e.target.value,
       })
     );
   }
@@ -74,43 +82,38 @@ export const Form = () => {
       ...input,
       type: e.target.value,
     });
-  }
-
-  function addsize(e) {
-    if (!input.size.includes(e.target.value)) {
-      setInput({
+    setErrors(
+      validate({
         ...input,
-        size: [...input.size, e.target.value],
-      });
-    }
+        [e.target.name]: e.target.value,
+      })
+    );
   }
 
-  function addcolor(e) {
-    if (!input.color.includes(e.target.value)) {
-      setInput({
-        ...input,
-        color: [...input.color, e.target.value],
-      });
-    }
-  }
-
-  function handleCloseSize(e) {
+  const uploadImage = (e) => {
     e.preventDefault();
-    const filter = input.size.filter((size) => size !== e.target.value);
-    setInput({
-      ...input,
-      size: filter,
-    });
-  }
-  function handleClosecolor(e) {
-    e.preventDefault();
-    const filterc = input.color.filter((size) => size !== e.target.value);
-    setInput({
-      ...input,
-      color: filterc,
-    });
-  }
+    const reader = new FileReader();
+    reader.readAsDataURL(e.target.files[0]);
+    reader.onloadend = async () => {
+      try {
+        axios
+          .post(
+            "http://localhost:3001/cloudinary",
+            { data: reader.result },
+            {
+              headers: {
+                "Content-Type": "application/json",
+              },
+            }
+          )
+          .then((res) => setInput({ ...input, images: [res.data] }));
+      } catch (error) {
+        console.error(error);
+      }
+    };
+  };
 
+  const classes = useStyles();
   return (
     <div>
       <div>
@@ -118,239 +121,204 @@ export const Form = () => {
       </div>
       <h1>Create New Product</h1>
 
-      <div>
-        <form onSubmit={(e) => handleSubmit(e)}>
-          <div>
-            <label> Name </label>
-            <input
-              value={input.name}
-              type="text"
+      <div
+        style={{
+          display: "flex",
+          justifyContent: "center",
+        }}
+      >
+        <div
+          style={{
+            display: "flex",
+            justifyContent: "center",
+            flexDirection: "column",
+            alignItems: "center",
+            backgroundColor: "white",
+            padding: 10,
+            width: "500px",
+            borderRadius: "20px",
+          }}
+        >
+          <FormControl margin="dense" sx={{ width: 400 }}>
+            <InputLabel>Category</InputLabel>
+            <Input
+              id="category"
+              onChange={handleChange}
+              name="category"
+              value={input.category}
+            />
+            {errors.category ? (
+              <FormHelperText id="category">{errors.category}</FormHelperText>
+            ) : (
+              false
+            )}
+          </FormControl>
+
+          <FormControl margin="dense" sx={{ width: 400 }}>
+            <InputLabel>Name</InputLabel>
+            <Input
+              id="name"
+              onChange={handleChange}
               name="name"
-              onChange={(e) => handleChange(e)}
-              className={!errors.name ? style.input : T.input}
+              value={input.name}
             />
-            {errors.name && (
-              <p
-                key={errors.name}
-                className={T.pError}
-                id="component-error-text"
-              >
-                {errors.name}
-              </p>
+            {errors.name ? (
+              <FormHelperText id="name">{errors.name}</FormHelperText>
+            ) : (
+              false
             )}
-          </div>
-          <div>
-            <label> descriptions </label>
-            <textarea
-              value={input.descriptions}
-              onChange={(e) => handleChange(e)}
-              rows="5"
-              cols="30"
+          </FormControl>
+
+          <FormControl margin="dense" sx={{ width: 400 }}>
+            <InputLabel>Descriptions</InputLabel>
+            <Input
+              id="descriptions"
+              onChange={handleChange}
               name="descriptions"
-              className={!errors.descriptions ? style.input : T.input}
+              value={input.descriptions}
             />
-            {errors.descriptions && (
-              <p
-                key={errors.descriptions}
-                className={T.pError}
-                id="component-error-text"
-              >
+            {errors.descriptions ? (
+              <FormHelperText id="descriptions">
                 {errors.descriptions}
-              </p>
+              </FormHelperText>
+            ) : (
+              false
             )}
-          </div>
+          </FormControl>
 
-          <div>
-            <label> Price </label>
-            <input
-              value={input.price}
-              id="standard-number"
+          <FormControl margin="dense" sx={{ width: 400 }}>
+            <InputLabel>Price</InputLabel>
+            <Input
               type="number"
-              onChange={(e) => handleChange(e)}
+              id="price"
+              onChange={handleChange}
               name="price"
-              className={!errors.price ? style.input : T.input}
+              value={input.price}
             />
-
-            {errors.price && (
-              <p
-                key={errors.price}
-                className={T.pError}
-                id="component-error-text"
-              >
-                {errors.price}
-              </p>
+            {errors.price ? (
+              <FormHelperText id="price">{errors.price}</FormHelperText>
+            ) : (
+              false
             )}
-          </div>
+          </FormControl>
 
-          <div>
-            <label> Stock </label>
-            <input
-              value={input.stock}
+          <FormControl margin="dense" sx={{ width: 400 }}>
+            <InputLabel>Stock</InputLabel>
+            <Input
               type="number"
-              onChange={(e) => handleChange(e)}
+              id="stock"
+              onChange={handleChange}
               name="stock"
-              className={!errors.stock ? style.input : T.input}
+              value={input.stock}
             />
-            {errors.stock && (
-              <p
-                key={errors.stock}
-                className={T.pError}
-                id="component-error-text"
-              >
-                {errors.stock}
-              </p>
+            {errors.stock ? (
+              <FormHelperText id="stock">{errors.stock}</FormHelperText>
+            ) : (
+              false
             )}
-          </div>
-          <div>
-            <label> Type </label>
-            <select
-              className={style.filterSelect}
+          </FormControl>
+
+          <FormControl variant="standard" sx={{ m: 1, minWidth: 200 }}>
+            <InputLabel id="Type">Type</InputLabel>
+            {errors.type ? (
+              <FormHelperText id="type">{errors.type}</FormHelperText>
+            ) : (
+              false
+            )}
+            <Select
+              labelId="Type"
+              id="Type"
               value={input.type}
               label="Type"
               onChange={handleTypes}
             >
-              <option> Choose Type </option>
-              <option value={"Shirt"}> Shirt </option>
-              <option value={"Sweather"}> Sweather </option>
-              <option value={"Other"}> Other </option>
-            </select>
-            {/* <Button variant="secondary" size="medium" sx={{width: 100, bgcolor: '#5b5b5b', border: 'black'}}> Add </Button> */}
-          </div>
-
-          <div>
-            <label> Size </label>
-            <select
-              multiple={false}
-              value={input.size}
-              label="Size"
-              onChange={(e) => {
-                addsize(e);
-              }}
-              className={style.filterSelect}
-            >
-              <option> Choose Size </option>
-              <option value={"XSmall"}> XSmall </option>
-              <option value={"Small"}> Small </option>
-              <option value={"Medium"}> Medium </option>
-              <option value={"Large"}> Large </option>
-              <option value={"XLarge"}> XLarge </option>
-              <option value={"XXLarge"}> XXLarge </option>
-            </select>
-          </div>
-          <div className={style.DivContainer}>
-            <div className={style.sizes}>
-              {input.size.length > 0 &&
-                input.size.map((c) => {
-                  return (
-                    <div key={c}>
-                      <button value={c} onClick={(e) => handleCloseSize(e)}>
-                        {" "}
-                        -{" "}
-                      </button>
-                      <p> {c} </p>
-                    </div>
-                  );
-                })}
-            </div>
-          </div>
-          <div>
-            <div>
-              <label> Color </label>
-              <select
-                className={style.filterSelect}
-                value={input.size}
-                label="Size"
-                onChange={(e) => {
-                  addcolor(e);
-                }}
-              >
-                <option> Choose Color </option>
-                <option value={"Black"}> Black </option>
-                <option value={"White"}> White </option>
-                <option value={"Pink"}> Pink </option>
-              </select>
-            </div>
-          </div>
-          <div className={style.DivContainer}>
-            <div className={style.sizes}>
-              {input.color.length > 0 &&
-                input.color.map((c) => {
-                  return (
-                    <div key={c}>
-                      <button value={c} onClick={(e) => handleClosecolor(e)}>
-                        {" "}
-                        -{" "}
-                      </button>
-                      <p> {c} </p>
-                    </div>
-                  );
-                })}
-            </div>
-          </div>
-          <div>
-            <label> Category </label>
-            <input
-              className={!errors.category ? style.input : T.input}
-              value={input.category}
-              type="text"
-              id="component-error"
-              label="Category"
-              name="category"
-              aria-describedby="component-error-text"
-              onChange={(e) => handleChange(e)}
-            />
-            {errors.category && (
-              <p
-                key={errors.category}
-                className={T.pError}
-                id="component-error-text"
-              >
-                {errors.category}
-              </p>
-            )}
-          </div>
-
-          <label> Images </label>
-
-          <div>
+              <MenuItem value={"Shirt"}>Shirt</MenuItem>
+              <MenuItem value={"Sweather"}>Sweather</MenuItem>
+              <MenuItem value={"Other"}>Other</MenuItem>
+            </Select>
+          </FormControl>
+        </div>
+      </div>
+      <div>
+        <label> Images </label>
+        <>
+          <div className={classes.root}>
             <input
               accept="image/*"
+              className={classes.input}
+              id="contained-button-file"
               multiple
               type="file"
-              onChange={(e) => handleImages(e)}
+              onChange={uploadImage}
             />
-            <div>
-              <img id="output" alt="imageProduct" width={200} />
-            </div>
-
-            {errors.images && (
-              <p
-                key={errors.images}
-                className={errors.images ? T.pError : null}
-                id="component-error-text"
-              >
-                {errors.images}
-              </p>
-            )}
+            <label htmlFor="contained-button-file">
+              <Button variant="contained" color="primary" component="span">
+                Upload
+              </Button>
+            </label>
           </div>
           <div>
-            {input.images && input.images.map((e) => <p key={e}> {e} </p>)}
+            {input.images.length ? (
+              <div className={style.imageDiv}>
+                <img src={input.images} alt="algo" height="200px" />
+                <FormControl
+                  onChange={addcolor}
+                  sx={{
+                    backgroundColor: "white",
+                    borderRadius: 3,
+                    margin: 3,
+                    padding: 3,
+                  }}
+                >
+                  <FormLabel id="demo-radio-buttons-group-label">
+                    Color
+                  </FormLabel>
+                  {errors.color ? (
+                    <FormHelperText id="color">{errors.color}</FormHelperText>
+                  ) : (
+                    false
+                  )}
+                  <RadioGroup
+                    sx={{ color: "black" }}
+                    aria-labelledby="demo-radio-buttons-group-label"
+                    name="radio-buttons-group"
+                  >
+                    <FormControlLabel
+                      value="Black"
+                      control={<Radio />}
+                      label="Negro"
+                    />
+                    <FormControlLabel
+                      value="White"
+                      control={<Radio />}
+                      label="Blanco"
+                    />
+                    <FormControlLabel
+                      value="Blue"
+                      control={<Radio />}
+                      label="Azul"
+                    />
+                    <FormControlLabel
+                      value="Pink"
+                      control={<Radio />}
+                      label="Rosado"
+                    />
+                  </RadioGroup>
+                </FormControl>
+              </div>
+            ) : (
+              false
+            )}
           </div>
-
-          <button
-            type="submit"
-            disabled={Object.keys(errors).length > 0 ? true : false}
-            className={style.button}
-          >
-            {" "}
-            Create Product
-          </button>
-        </form>
-
-        <Link to="/home">
-          <button className={style.button}> Back to Home</button>
-        </Link>
+        </>
       </div>
+      <button
+        onClick={handleSubmit}
+        disabled={Object.keys(errors).length > 0 ? true : false}
+        className={style.button}
+      >
+        Create Product
+      </button>
     </div>
   );
 };
