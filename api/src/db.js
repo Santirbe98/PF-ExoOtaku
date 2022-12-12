e7eff010d424afa3207100e78765355eeb9bfff4
Santirbe98 <santirbe@gmail.com>
2022-12-12 16:07:35 -0300

    feat(DEPLOY)

--- api/src/db.js
+++ api/src/db.js
@@ -2,7 +2,14 @@ require("dotenv").config();
 const { Sequelize } = require("sequelize");
 const fs = require("fs");
 const path = require("path");
-const { DB_USER, DB_PASSWORD, DB_HOST } = process.env;
+const { DB_USER, DB_PASSWORD, DB_HOST, DB_URL } = process.env;
+
+// Conexion para RAILWAY descomentar a la hora de hacer el deploy y si queren usarlo localmente usen el new sequelize de abajo
+
+// const sequelize = new Sequelize(DB_URL, {
+//   logging: false,
+//   native: false,
+// });
 
 const sequelize = new Sequelize(
   `postgres://${DB_USER}:${DB_PASSWORD}@${DB_HOST}/exOtaku`,
