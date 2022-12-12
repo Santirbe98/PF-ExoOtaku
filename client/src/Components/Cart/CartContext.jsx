f629d48dff03168af4393163f89987764fb0e51e
Facuzanan88 <facundozanandrea@gmail.com>
2022-12-12 20:25:20 -0300

    NO ACEPTAR

--- client/src/Components/Cart/CartContext.jsx
+++ client/src/Components/Cart/CartContext.jsx
@@ -17,34 +17,42 @@ export const CartProvider = ({ children }) => {
     }, [cartItems]);
 
     const addItemToCart = (product) => {
-        const inCart = cartItems.find((p) => p.id === product.id);
+        const inCart = cartItems.find((p) => p.id === product.id && p.size === product.size);
         if (inCart) {
-            setCartItems(
-                cartItems.map((p) => {
-                    if (p.id === product.id) {
-                        return { ...inCart, amount: inCart.amount + 1 };
-                    } else return p;
-                })
-            );
-        } else {
-            setCartItems([...cartItems, { ...product, amount: 1 }]);
+            return setCartItems([...cartItems, { ...inCart, amount: inCart.amount + 1 }])
         }
-    };
+        const inCartdiferentSize = cartItems.find((p) => p.id === product.id && p.size !== product.size);
+        if (inCartdiferentSize) {
+            return setCartItems([...cartItems, { ...product, amount: 1 }])
+        }
+        setCartItems([...cartItems, { ...product, amount: 1 }]);
+    }
+
+    /*     const deleteItemToCart = (product) => {
+            const inCart = cartItems.find((p) => p.id === product.id);
+            if (inCart.amount === 1) {
+                setCartItems(cartItems.filter((p) => p.id !== product.id));
+            } else {
+                setCartItems(
+                    cartItems.map((p) => {
+                        if (p.id === product.id) {
+                            return { ...inCart, amount: inCart.amount - 1 };
+                        } else return p;
+                    })
+                );
+            }
+        }; */
 
     const deleteItemToCart = (product) => {
-        const inCart = cartItems.find((p) => p.id === product.id);
-        if (inCart.amount === 1) {
-            setCartItems(cartItems.filter((p) => p.id !== product.id));
-        } else {
-            setCartItems(
-                cartItems.map((p) => {
-                    if (p.id === product.id) {
-                        return { ...inCart, amount: inCart.amount - 1 };
-                    } else return p;
-                })
-            );
+        const inCart = cartItems.find((p) => p.id === product.id && p.size === product.size);
+        if (inCart && inCart.amount === 1) {
+            return setCartItems(cartItems.filter((p) => p.id !== product.id));
         }
-    };
+        if (inCart && inCart.amount > 1) {
+            return setCartItems({ ...inCart, amount: inCart.amount - 1 });
+        }
+    }
+
     return (
         <CartContext.Provider
             value={{ cartItems, addItemToCart, deleteItemToCart }}
