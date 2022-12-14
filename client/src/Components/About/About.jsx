20b88fe88539ccc7ca0ce39a28806987006d9cfc
Salvaarmanasco <salvador.armanasco@gmail.com>
2022-12-14 20:52:21 -0300

    responsive-about-salvaarmanasco

--- client/src/Components/About/About.jsx
+++ client/src/Components/About/About.jsx
@@ -5,9 +5,10 @@ import img3 from "../../Resources/uvjet-impresora-dtg-easy-tx-04-500x571.jpg";
 import { NavBar } from "../NavBar/NavBar.jsx";
 import { Footer } from "../Footer/Footer.jsx";
 import Carousel from "react-material-ui-carousel";
-import { Container, Grid, Box, Typography } from "@mui/material";
+import { Container, Box, Typography } from "@mui/material";
+import Grid from "@mui/material/Unstable_Grid2";
 
-export function About() {
+export const About = () => {
   let mock = [img1, img2, img3];
   const [actImg, setActImg] = useState(0);
   const quantity = mock?.length;
@@ -21,58 +22,58 @@ export function About() {
 
   return (
     <div>
-      <NavBar />
-      <Box>
-        <Container>
-          <Grid pt={5}>
-            <Typography variant="h4">Sobre Nosotros</Typography>
-          </Grid>
-          <Grid pt={4}>
-            <Typography variant="h5">Como hacemos nuestras prendas?</Typography>
-          </Grid>
-          <Grid pt={4} pb={4}>
-            <Typography variant="h6">
-              Nuestra ropa se realiza mediante impresoras DTG
-            </Typography>
-          </Grid>
-        </Container>
-      </Box>
-      <Carousel next={nextImg} prev={prevImg}>
-        {mock?.map((img, index) => (
-          <img key={index} src={img} alt="about" width="400" height="450" />
-        ))}
-      </Carousel>
-      <Box>
-        <Container>
-          <Grid pt={5}>
-            <Typography variant="h6">
-              Aseguramos un excelente producto 😎
-            </Typography>
-          </Grid>
-          <Grid pt={4}>
-            <Typography variant="h6">Especificaciones:</Typography>
-          </Grid>
-          <Grid pb={4}>
-            <Typography variant="subtitle1">
-              <ul style={{ listStyle: "none" }}>
-                <li key={1}>Utiliza tinta duopoint (USA).</li>
-                <li key={2}>Es resistente a lavados y el sol</li>
-                <li key={3}>
-                  Recien partir del lavado n°30 comienza a perder el brillo
-                </li>
-                <li key={4}>
-                  Su calidad es superior en compararcion con la serigrafia!
-                </li>
-                <li key={5}>
-                  Es una tinta Ecologica ya que esta compuesta con agua!
-                </li>
-                <li key={6}>Nuestros productos son 100% Algodon!</li>
-              </ul>
-            </Typography>
-          </Grid>
-        </Container>
-      </Box>
+      {/* <NavBar /> */}
+      <Grid container spacing={2}>
+        <Grid xs={12} md={12} lg={12} xl={12}>
+          <Typography variant="h4">Sobre Nosotros</Typography>
+        </Grid>
+        <Grid xs={12} md={12} lg={12} xl={12}>
+          <Typography variant="h4">Como hacemos nuestras prendas?</Typography>
+        </Grid>
+        <Grid xs={12} md={12} lg={12} xl={12}>
+          <Carousel next={nextImg} prev={prevImg}>
+            {mock?.map((img, index) => (
+              <img key={index} src={img} alt="about" width="400" height="450" />
+            ))}
+          </Carousel>
+        </Grid>
+      </Grid>
+      {/* //---------------------------------------------------------- */}
+      <Grid container spacing={1} marginLeft={5} marginRight={5}>
+        <Grid xs={12}>
+          <Typography variant="h4">Especificaciones:</Typography>
+        </Grid>
+        <Grid xs={12} md={12} lg={6} xl={4}>
+          <h4 key={1}>Utiliza tinta duopoint (USA).</h4>
+        </Grid>
+        <Grid xs={12} md={12} lg={6} xl={4}>
+          <h4 key={2}>Es resistente a lavados y el sol</h4>
+        </Grid>
+        <Grid xs={12} md={12} lg={6} xl={4}>
+          <h4 key={3}>
+            Recien partir del lavado n°30 comienza a perder el brillo
+          </h4>
+        </Grid>
+        <Grid xs={12} md={12} lg={6} xl={4}>
+          <h4 key={4}>
+            Su calidad es superior en compararcion con la serigrafia!
+          </h4>
+        </Grid>
+        <Grid xs={12} md={12} lg={6} xl={4}>
+          <h4 key={5}>
+            Es una tinta Ecologica ya que esta compuesta con agua!
+          </h4>
+        </Grid>
+        <Grid xs={12} md={12} lg={6} xl={4}>
+          <h4 key={6}>Nuestros productos son 100% Algodon!</h4>
+        </Grid>
+      </Grid>
+      <Grid container spacing={1}>
+        {/* <Typography variant="subtitle1"> */}
+
+        {/* </Typography> */}
+      </Grid>
       <Footer />
     </div>
   );
-}
+};
