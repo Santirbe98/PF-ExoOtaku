d92e41607822da7779bf521fcde6c7cb416b1fba
Salvaarmanasco <salvador.armanasco@gmail.com>
2023-01-07 17:02:57 -0300

    fix-responsive-landing

--- client/src/Components/LandingPage/LandingPage.jsx
+++ client/src/Components/LandingPage/LandingPage.jsx
@@ -1,4 +1,4 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 import { useDispatch } from "react-redux";
 import { getProducts, orderByRank, orderByDate } from "../../Redux/Actions";
 import {
@@ -11,7 +11,7 @@ import {
 } from "@mui/material";
 import { TopRanked } from "../TopRanked/TopRanked";
 import { Link } from "react-router-dom";
-import { TopDate } from "../TopDate/TopDate"
+import { TopDate } from "../TopDate/TopDate";
 export const LandingPage = () => {
   let dispatch = useDispatch();
 
@@ -19,12 +19,27 @@ export const LandingPage = () => {
     dispatch(getProducts())
       .then(() => dispatch(orderByRank()))
       .then(() => dispatch(orderByDate()));
-
   }, [dispatch]);
+
+  const [width, setWidth] = useState(window.innerWidth);
+  const [height, setHeight] = useState(window.innerHeight);
+
+  const handleResize = () => {
+    setWidth(window.innerWidth);
+    setHeight(window.innerHeight);
+  };
+
+  useEffect(() => {
+    window.addEventListener("resize", handleResize);
+  }, []);
 
   return (
     <Box>
-      <Typography variant="h3">ExoOtaku anime top 5</Typography>
+      <Grid container>
+        <Grid xs={12} sm={12} md={12} lg={12} xl={12} margin={1}>
+          <Typography variant="h3">ExoOtaku anime top 5</Typography>
+        </Grid>
+      </Grid>
       <Box
         sx={{
           display: "flex",
@@ -43,57 +58,77 @@ export const LandingPage = () => {
         color="white"
         sx={{ marginTop: 3, width: "90%", marginLeft: "5%" }}
       />
-      <Box
-        sx={{
-          marginTop: 3,
-        }}
-      >
-        <Grid container>
-          <Grid item xs={12} sm={12} md={6}>
-            <Typography variant="h5">
-              Queres saber de nuestras nuevas ofertas?
-            </Typography>
-            <Typography variant="subtitle2">
-              Dejanos tu email para robar todos tus datos y los de tus seres
-              queridos
-            </Typography>
-          </Grid>
-          <Grid item xs={12} sm={12} md={6}>
-            <Box
-              sx={{
-                display: "flex",
-                justifyContent: "center",
-                padding: "0px 10px 0px 10px",
-              }}
-            >
-              <TextField
-                placeholder="E-mail"
-                color="success"
-                style={{
-                  backgroundColor: "rgba(255, 253, 253, 0.900)",
-                  borderRadius: "10px",
-                  width: 450,
-                }}
-                focused
-              />
-              <Button
-                variant="contained"
-                color="success"
-                sx={{ marginLeft: 3 }}
-              >
-                Subcribirme
-              </Button>
-            </Box>
-          </Grid>
+      {/* ------------------------------------------------------------- */}
+      <Grid container>
+        <Grid xs={12} sm={12} md={12} lg={12} xl={12} mb={3} mt={3}>
+          <Typography variant="h5">
+            Queres saber de nuestras nuevas ofertas?
+          </Typography>
+          <Typography variant="subtitle2">
+            Dejanos tu email para robar todos tus datos y los de tus seres
+            queridos
+          </Typography>
         </Grid>
-        <Divider
-          color="white"
-          sx={{ marginTop: 3, width: "90%", marginLeft: "5%" }}
-        />
-        <Typography variant="h3" mt={10}>
-          Nuevos Productos y Ofertas
-        </Typography>
-        <Box
+        <Grid xs={12} sm={12} md={12} lg={6} xl={7}>
+          <Box
+            sx={
+              width > 1200
+                ? {
+                    display: "flex",
+                    justifyContent: "flex-end",
+                  }
+                : { display: "flex", justifyContent: "center" }
+            }
+          >
+            <TextField
+              placeholder="E-mail"
+              color="success"
+              style={
+                width < 1200
+                  ? {
+                      backgroundColor: "rgba(255, 253, 253, 0.900)",
+                      borderRadius: "10px",
+                      width: 400,
+                    }
+                  : {
+                      backgroundColor: "rgba(255, 253, 253, 0.900)",
+                      borderRadius: "10px",
+                      width: 400,
+                    }
+              }
+              focused
+            />
+          </Box>
+        </Grid>
+        <Grid xs={12} sm={12} md={12} lg={6} xl={5}>
+          <Box
+            sx={
+              width > 1200
+                ? {
+                    display: "flex",
+                    justifyContent: "flex-start",
+                  }
+                : { display: "flex", justifyContent: "center" }
+            }
+          >
+            <Button
+              variant="contained"
+              color="success"
+              sx={{ padding: "15px", ml: "10px" }}
+            >
+              Subcribirme
+            </Button>
+          </Box>
+        </Grid>
+      </Grid>
+      <Divider
+        color="white"
+        sx={{ marginTop: 3, width: "90%", marginLeft: "5%" }}
+      />
+      <Typography variant="h3" mt={10}>
+        Nuevos Productos y Ofertas
+      </Typography>
+      <Box
         sx={{
           display: "flex",
           justifyContent: "center",
@@ -101,7 +136,6 @@ export const LandingPage = () => {
         }}
       >
         <TopDate />
-      </Box>
       </Box>
     </Box>
   );
